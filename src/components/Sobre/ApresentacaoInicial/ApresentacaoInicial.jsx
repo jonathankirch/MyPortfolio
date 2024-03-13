8d6056e99f93df589dff51c9679f4c46d05f8911
Jonathan Kirch <jonathankirch101@gmail.com>
2024-03-13 19:23:32 -0300

    add os textos sobre mim e o icone do bootstrap que faltava

--- src/components/Sobre/ApresentacaoInicial/ApresentacaoInicial.jsx
+++ src/components/Sobre/ApresentacaoInicial/ApresentacaoInicial.jsx
@@ -3,8 +3,8 @@ import simbolo from './img/simbolo.png';
 export const ApresentacaoInicial = () => {
 	return (
 		<>
-			<div className='pt-5' id='home'>
-				<section className='container border border-dark shadow rounded w-75 mx-auto my-5 p-4 bg-dark-secondary'>
+			<div className='pt-5 px-3' id='home'>
+				<section className='container-sm border border-dark shadow rounded  mx-auto my-5 p-4 bg-dark-secondary'>
 					<div className='row row-cols-1 row-cols-md-2'>
 						<div className='col'>
 							<div className='w-50 mx-auto'>
@@ -18,11 +18,11 @@ export const ApresentacaoInicial = () => {
 						</div>
 						<div className='col position-relative'>
 							<h1 className='text-light fw-bold'>Eu</h1>
-							<p className='text-gray'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
-								nemo quos. Ipsum dignissimos obcaecati minima beatae commodi
-								sint labore, porro alias error perspiciatis sapiente voluptatem
-								accusantium consectetur sit neque voluptas.
+							<p className='text-gray fs-5'>
+								Meu nome é Jonathan e estudo programação há cerca de dois anos.
+								Iniciei minha jornada durante o primeiro ano do ensino médio e
+								desde então, meu interesse pela tecnologia só cresceu. Descobri
+								na programação que de fato este é o caminho que quero seguir.
 							</p>
 							<div className='position-absolute bottom-0 end-0 position-relative me-2 p-2 shadow-sm rounded'>
 								<img style={{ width: 30 }} src={simbolo} alt='</>' />
@@ -30,17 +30,21 @@ export const ApresentacaoInicial = () => {
 						</div>
 					</div>
 				</section>
-				<section className='container border border-dark shadow rounded w-75 mx-auto my-5 p-4 bg-dark-secondary'>
+				<section className='container-sm border border-dark shadow rounded mx-auto my-5 p-4 bg-dark-secondary'>
 					<div className='row row-cols-1 row-cols-md-2'>
 						<div className='col position-relative'>
 							<h1 className='text-light fw-bold' id='sobre-mim'>
 								Sobre mim
 							</h1>
-							<p className='text-gray'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
-								nemo quos. Ipsum dignissimos obcaecati minima beatae commodi
-								sint labore, porro alias error perspiciatis sapiente voluptatem
-								accusantium consectetur sit neque voluptas.
+							<p className='text-gray fs-5'>
+								Gosto de aprender coisas novas, especialmente aquelas que
+								aprimoram a qualidade dos meus projetos. Estou constantemente em
+								busca de conhecimento para expandir minhas habilidades e
+								enfrentar desafios com confiança.
+							</p>
+							<p className='text-gray fs-5'>
+								Abaixo, você encontrará uma lista das tecnologias, frameworks e
+								linguagens que estudei e dominei ao longo desta jornada.
 							</p>
 						</div>
 						<div className='col position-relative'>
--- src/components/Sobre/Tecnologias/Tecnologias.jsx
+++ src/components/Sobre/Tecnologias/Tecnologias.jsx
@@ -2,27 +2,29 @@ import { FaHtml5 } from 'react-icons/fa';
 import { FaCss3Alt } from 'react-icons/fa';
 import { RiJavascriptFill } from 'react-icons/ri';
 import { FaReact } from 'react-icons/fa';
+import { FaBootstrap } from "react-icons/fa";
 
 export const Tecnologias = () => {
 	const tecnologias = [
 		{ name: 'HTML5', img: <FaHtml5 size='50' color='red' className='rounded shadow bg-dark-transparent'/> },
 		{ name: 'CSS3', img: <FaCss3Alt size='50' color='blue' className='rounded shadow bg-dark-transparent'/> },
 		{ name: 'JS', img: <RiJavascriptFill size='50' color='yellow'className='rounded shadow bg-dark-transparent'/> },
-		{ name: 'React', img: <FaReact size='50' color='blue'className='rounded shadow bg-dark-transparent'/> },
+		{ name: 'React', img: <FaReact size='50' color='rgb(94, 211, 244)'className='rounded shadow bg-dark-transparent'/> },
+		{ name: 'Bootstrap', img: <FaBootstrap size='50' color='rgb(135, 18, 247)'className='rounded shadow bg-dark-transparent'/> },
 
 
 	];
 
 	return (
 		<section className='container text-center pb-5'>
-			<h1 className='container bg-dark-transparent w-50 rounded text-light mb-4 text-purple shadow p-2 fw-bold'>Tecnologias</h1>
+			<h1 className='container bg-dark-transparent w-50 rounded text-light mb-4 text-purple shadow p-2 fw-bold'>Tecnologias e Frameworks</h1>
 			<div className='row'>
 				<ul className='d-flex d-flex justify-content-center flex-wrap list-unstyled'>
-					{tecnologias &&
+					{tecnologias&&
 						tecnologias.map((tecnologia, id) => (
 							<li key={id} title={tecnologia.name} className='mx-4'>
 									{tecnologia.img}
-									<p className='text-purple'>{tecnologia.name}</p>
+									<p className='text-light'>{tecnologia.name}</p>
 							</li>
 						))}
 				</ul>
